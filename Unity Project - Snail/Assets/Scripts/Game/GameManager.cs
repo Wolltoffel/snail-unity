a6cb76b9b6c75d6777cc920bad5d82ae5bd21edf
Wolltoffel <lolo4draco@gmail.com>
2023-04-16 22:49:35 +0200

    Cleaned up some code

--- Unity Project - Snail/Assets/Scripts/Game/GameManager.cs
+++ Unity Project - Snail/Assets/Scripts/Game/GameManager.cs
@@ -5,6 +5,8 @@ using System;
 
 public class GameManager : MonoBehaviour
 {
+    public static event EventHandler<StatData> endGame;
+
     private void Start()
     {
 
@@ -21,7 +23,7 @@ public class GameManager : MonoBehaviour
             if (actionInfo.action != ActionInfo.Action.slide)
                 actionInfo.action = ActionInfo.Action.empty;
 
-            Tile nextSlideTile = previousTile.giveNextSlideTile(tile, activePlayer);;
+            Tile nextSlideTile = previousTile.giveNextSlideTile(tile, activePlayer);
             if (nextSlideTile != null)
             {   actionInfo.action = ActionInfo.Action.slide;
                 movePlayer(activePlayer, nextSlideTile,actionInfo);
@@ -30,21 +32,22 @@ public class GameManager : MonoBehaviour
         }
         
         RoundManager.switchTurnsEvent(actionInfo);
-
     }
 
     public static void EndGame()
     {
-        Debug.Log("End Game");
+        /*StatData stats = new StatData(RoundManager.roundCounter,);
+        endGame.Invoke(null,stats);*/
     }
 
+    //static Player winner()
+    //{
+       /* List<Player> players = PlayerConfig.player;
+        Player winner = players[0];
+        for(int i=0;i<players.Capacity;i++)
+        {
+            if (players[i].score>players[i+1].score)
 
+        }*/
+    }
 
-
-
-
-
-
-
-
-}
