41cc4ce9769866c2daa04bfd16e44e1cf726c21c
Wolltoffel <lzandianpour@stud.macromedia.de>
2023-04-20 01:59:53 +0200

    Active Players is now being highlighted

--- Unity Project - Snail/Assets/Scripts/Game/UI/PlayerNameUI.cs
+++ Unity Project - Snail/Assets/Scripts/Game/UI/PlayerNameUI.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class PlayerNameUI : MonoBehaviour
 {
     [SerializeField] int playerIndex;
+    [SerializeField] Color highlightColor;
+    Color defaultColor;
     Player player;
     TextMeshProUGUI tmpProText;
 
@@ -13,5 +16,16 @@ public class PlayerNameUI : MonoBehaviour
         player = PlayerManager.player[playerIndex];
         tmpProText = GetComponent<TextMeshProUGUI>();
         tmpProText.text = player.name;
+        defaultColor = tmpProText.color;
+        RoundManager.switchTurn += changeColor;
+        changeColor(null, EventArgs.Empty);
+    }
+
+    void changeColor(object sender, EventArgs e)
+    {
+        if (player.turn)
+            tmpProText.color = highlightColor;
+        else
+            tmpProText.color = defaultColor;
     }
 }
