9b1619de5a85ba9efe5e37a71ff6d10dc01b4417
Wolltoffel <lzandianpour@stud.macromedia.de>
2023-06-03 02:16:21 +0200

    Update Highlight.cs

--- Unity Project - Snail _ Rework/Assets/Scripts/Highlight.cs
+++ Unity Project - Snail _ Rework/Assets/Scripts/Highlight.cs
@@ -6,9 +6,17 @@ using UnityEngine.EventSystems;
 public class Highlight : MonoBehaviour, IPointerDownHandler
 {
     Vector2Int position;
+    GameManager manager;
+
+    public void InsertData(Vector2Int position,GameManager manager)
+    {
+        this.position = position;
+        this.manager = manager;
+    }
 
     void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
     {
-        
+        PlayerAction agentMove = new PlayerAction(ActionType.Move, position);
+        manager.MakeMove(agentMove);
     }
 }
