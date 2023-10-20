4141bd3f5b29263d7894c0a3f1553a2a9523f4a3
Wolltoffel <lolo4draco@gmail.com>
2023-10-20 16:52:21 +0200

    Clean up files

--- Unity Project - Snail/Assets/Scripts/Game/Player/ActionType.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public enum ActionType
-{
-    slide, skip, capture,surrender, empty
-};
--- Unity Project - Snail/Assets/Scripts/Game/Stats/ResultInfo.cs
+++
@@ -1,4 +0,0 @@
-public enum ResultInfo
-{
-    empty,score,surrender,maxTurnsWithoutCaptureExceeded
-}
